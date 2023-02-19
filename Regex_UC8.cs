223e64559b505a2e0092d7fed8ec8d645680aa24
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-02-19 14:19:38 +0530

    Add files via upload

---
+++ Regex_UC8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexAssig
+{
+    public class Regex_UC8
+    {
+        public static string REGEX_PATTERN = "^([a-zA-Z0-9])*[!@#$%^&*]{1}{1}([a-zA-Z0-9])*$";
+
+        public bool validateName(string name)
+        {
+            return Regex.IsMatch(name, REGEX_PATTERN);
+        }
+    }
+}
