fd219313166b176112882cdbd82c1c00e4b235ca
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-02-19 14:10:46 +0530

    Add files via upload

---
+++ Regex_UC2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexAssig
+{
+    public class Regex_UC2
+    {
+        public static string REGEX_PATTERN = "^[a-zA-Z]{2}[A-Z]{1}$";
+
+        public bool validateName(string name)
+        {
+            return Regex.IsMatch(name, REGEX_PATTERN);
+        }
+    }
+}
